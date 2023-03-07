39029feacfcb31def9539e79af53c7ae333f88a4
JerryFans <fanjiarong_haohao@163.com>
2023-03-07 20:34:43 +0800

    fix

--- hello_chat_gpt.rb
+++ hello_chat_gpt.rb
@@ -1,33 +1,23 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class HelloChatGpt < Formula
-  desc ""
-  homepage ""
-  url "https://github.com/JerryFans/hello_chat_gpt/raw/main/dist/1.0.0/hello_chat_gpt-1.0.0.tar.gz"
-  sha256 "99aac8eaeb363485df39d4a6796307d98f260ce5a2b42fd2769bff04e3632843"
-  license ""
+  desc "A command-line tool that generates chat responses using OpenAI's GPT model."
+  homepage "https://github.com/JerryFans/hello_chat_gpt"
+  url "https://github.com/JerryFans/hello_chat_gpt/raw/main/release/v1.0/hello_chat_gpt_intel"
+  sha256 "ace62092ac76b7c25caca467ff71e1300b68ba023f8c55f45956eb6dc20a06fc"
 
-  # depends_on "cmake" => :build
+  if Hardware::CPU.arm?
+    url "https://github.com/JerryFans/hello_chat_gpt/raw/main/release/v1.0/hello_chat_gpt_m1"
+    sha256 "5befc858818e46afda39edcee30d70c96d339bbf2ef73d83fb16b0e91747bb6d"
+  end
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", *std_configure_args, "--disable-silent-rules"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    if Hardware::CPU.arm?
+      bin.install "hello_chat_gpt_m1" => "hello_chat_gpt"
+    else
+      bin.install "hello_chat_gpt_intel" => "hello_chat_gpt"
+    end
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test hello_chat_gpt`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "#{bin}/hello_chat_gpt", "--help"
   end
 end
